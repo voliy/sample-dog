2a4fe98d233e8a0b9620a1bf5c5e69686f127137
voliy <volconfiy@gmail.com>
2016-08-14 15:32:20 +0300

    Improved DAO layer

--- src/main/java/voliy/samples/dog/dao/DogDao.java
+++ src/main/java/voliy/samples/dog/dao/DogDao.java
@@ -2,13 +2,20 @@ package voliy.samples.dog.dao;
 
 import voliy.samples.dog.model.Dog;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class DogDao {
-    private Map<Integer, Dog> dogs = new LinkedHashMap<Integer, Dog>();
+    private volatile int nextId;
+    private Map<Integer, Dog> dogs = new LinkedHashMap<>();
 
     public Collection<Dog> dogs() {
         return dogs.values();
+    }
+
+    public void init() {
+        Dog.samples().forEach(this::add);
     }
 
     public Dog get(int id) {
@@ -17,22 +24,12 @@ public class DogDao {
 
     public void add(Dog dog) {
         if (dog.getId() == null) {
-            dog.setId(nextId());
+            dog.setId(++nextId);
             dogs.put(dog.getId(), dog);
         }
     }
 
     public void delete(int id) {
         dogs.remove(id);
-    }
-
-    public void init() {
-        for (Dog dog : Dog.samples()) {
-            add(dog);
-        }
-    }
-
-    private int nextId() {
-        return dogs.isEmpty() ? 1 : Collections.max(dogs.keySet()) + 1;
     }
 }
