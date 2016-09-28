3ad9d0153a3162700d82628b10d59829fa6987a4
voliy <volconfiy@gmail.com>
2016-09-28 16:17:22 +0300

    Load-time weaving with AspectJ

--- src/main/java/foo/Main.java
+++ src/main/java/foo/Main.java
@@ -5,13 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public final class Main {
     // -javaagent:D:/projects/trainings/sample-dog/target/sample-dog-1.0.0/WEB-INF/lib/spring-instrument-4.3.2.RELEASE.jar
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml", Main.class);
 
         StubEntitlementCalculationService entitlementCalculationService
                 = (StubEntitlementCalculationService) ctx.getBean("entitlementCalculationService");
 
         // the profiling aspect is 'woven' around this method execution
-        entitlementCalculationService.test();
+        entitlementCalculationService.test1();
+
+        entitlementCalculationService.test2();
     }
 }
--- src/main/java/foo/StubEntitlementCalculationService.java
+++ src/main/java/foo/StubEntitlementCalculationService.java
@@ -1,7 +1,15 @@
 package foo;
 
 public class StubEntitlementCalculationService {
-    public void test() {
-        System.out.println("test");
+    void test1() throws Exception {
+        System.out.println("test1 start");
+        Thread.sleep(1234);
+        System.out.println("test1 end");
+    }
+
+    void test2() throws Exception {
+        System.out.println("test2 start");
+        Thread.sleep(2000);
+        System.out.println("test2 end");
     }
 }
